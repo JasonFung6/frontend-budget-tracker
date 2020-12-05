5536bdd17e0c24c0cd2acdfee2d38f64ad611fe5
Siu Kei Tam <tam.s.k.92@gmail.com>
2020-12-05 16:08:14 +0000

    Setup of page components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Expenditure from './components/Expenditure'
+import SpendInput from './components/SpendInput'
 
 const App = () => {
   return (
