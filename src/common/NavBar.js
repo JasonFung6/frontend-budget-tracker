9f968afe7b9c1586e54f0bc85d5c437d80c97585
Siu Kei Tam <tam.s.k.92@gmail.com>
2020-12-27 12:38:01 +0000

    Completed the transaction summary card on the home page

--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import { Navbar, Nav, Image } from 'react-bootstrap'
+
 
 const NavBar = () => {
 
@@ -10,12 +12,25 @@ const NavBar = () => {
   // }
 
   return (
-    <nav className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/profile'>Profile</Link>
-      <Link to='/expenses'>Expenses</Link>
-      <Link to='/summary'>Summary</Link>
-    </nav>
+    <Navbar bg="nav-theme" expand="lg" variant="dark">
+      <Navbar.Brand as={Link} to='/profile'>
+        <Image src='https://pbs.twimg.com/profile_images/3591982071/f875f1abcf63e927a81cd28d375060a5.jpeg'
+          roundedCircle
+          width='50'
+          height='50'
+          alt='Image of User'
+        /> Hi, Suzy
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" >
+        <Nav className="mx-auto">
+          <Nav.Link as={Link} to='/'>Home</Nav.Link>
+          <Nav.Link as={Link} to='/profile'>Profile</Nav.Link>
+          <Nav.Link as={Link} to='/expenses'>Expenses</Nav.Link>
+          <Nav.Link as={Link} to='/summary'>Summary</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
 
   )
 }
