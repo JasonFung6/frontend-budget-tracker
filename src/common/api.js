6aeba9ddf74dad7e2d0f7b1795bc7a5419b7b1d5
Siu Kei Tam <tam.s.k.92@gmail.com>
2021-02-13 12:22:11 +0000

    tried to add allow Access-Control-Allow-Origin header in FE but seems like has to be sorted on the BE

--- src/common/api.js
+++ src/common/api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 // const APIUrl = 'http://api.jasonfung.co.uk/budgettracker/?apiType=multi&accId=0014K00000BWiJyQAL&orderBy=ascending&limit=10'
 
+
 //GET all expenditure request
 export const getAllExpenditure = (accID, transactionNum) => {
   const res = axios.get(`https://api.jasonfung.co.uk/budgettracker/?apiType=multi&accId=${accID}&orderBy=ascending&limit=${transactionNum}`)
